3b6755f8aafe265818637a9dd777b76a10128b09
lasquick <59918249+lasquick@users.noreply.github.com>
2020-04-25 19:47:55 -0400

    Redesigned API request to use single input request to pull county data.

--- collect-povtop10-county.py
+++ collect-povtop10-county.py
@@ -4,10 +4,15 @@ Created on Fri Mar 27 15:25:37 2020
 
 @author: lasqu
 """
-#This API request for each county is done manually.
+#This API request for each county is done manually. Change the county list
+#and rank here and the rest of the file will automatically run and save.
 
 import requests
 import pandas as pd
+
+#!!IMPORTANT: CHANGE TO CORRECT POVERTY RANKING LIST AND COUNTY RANK!!
+county_list = 'povtop10'
+county_rank = '1'
 
 #Uses CSV file with Census data variable names to create data pull request.
 var_info = pd.read_csv('county.csv')
@@ -15,15 +20,20 @@ var_name = var_info['variable'].to_list()
 var_list = ['NAME']+var_name
 var_string = ','.join(var_list)
 
-county_info = pd.read_csv('povtop10.csv')
-in_list = ['county:'+county_info['county'].astype(str)+' state:'+county_info['state'].astype(str)]
+county_info = pd.read_csv(county_list+'.csv')
+
+#Create in_list to identify top ten counties' county and state IDs.
+#Would be useful to turn this into an API-pull for-loop in the future.
+in_list = ('county:'+county_info['county'].astype(str).str.zfill(3)+' state:'\
+           +county_info['state'].astype(str).str.zfill(2))
 print(in_list)
+index_rank = int(county_rank)-1
+
 
 #Sets API observation capture conditions and sets API key.
 api = 'https://api.census.gov/data/2018/acs/acs5'
 for_clause = 'tract:*'
-#in_clause = in_list
-in_clause = 'county:071 state:46'
+in_clause = in_list[index_rank]
 key_value = '7106dbed1eb3f120e22e544652313d756577a258'
 
 
@@ -44,5 +54,4 @@ attain = pd.DataFrame(columns=colnames, data=datarows)
 
 #Sets name as the index and writes to CSV file.
 attain.set_index('NAME', inplace=True)
-attain.to_csv('census-povtop10-3.csv')
-    
+attain.to_csv('census-'+county_list+'-'+county_rank+'.csv')
